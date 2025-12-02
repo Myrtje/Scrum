273e2db5c5ea4ba27c2e9b1b96e13d7b7bdedfaa
nom <nometox@gmail.com>
2025-12-02 14:27:27 +0100

    player animation

--- game/Assets/Player/PlayerController.cs
+++ game/Assets/Player/PlayerController.cs
@@ -8,13 +8,16 @@ namespace game.PlayerControl
 {
     public class PlayerController : MonoBehaviour
     {
-        private Rigidbody2D _rb;
-        private InputManager _inputM;
+        Rigidbody2D _rb;
+        InputManager _inputM;
         [SerializeField] private float moveSpeed;
+        Animator anim;
+
         private void Start()
         {
             _rb = GetComponent<Rigidbody2D>();
             _inputM = GetComponent<InputManager>();
+            anim = GetComponent<Animator>();
         }
 
         private void FixedUpdate()
@@ -25,6 +28,10 @@ namespace game.PlayerControl
         private void Move()
         {
             _rb.linearVelocity = _inputM.Move * moveSpeed;
+            anim.SetBool("WalkingUp", _inputM.Move == Vector2.up);
+            anim.SetBool("WalkingDown", _inputM.Move == Vector2.down);
+            anim.SetBool("WalkingLeft", _inputM.Move == Vector2.left);
+            anim.SetBool("WalkingRight", _inputM.Move == Vector2.right);
         }
     }
 }
